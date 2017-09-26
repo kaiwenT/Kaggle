f40e859248c4682792697b6d767ac053a97e9371
kaiwenT <1043595701@qq.com>
2017-09-26 20:38:52 +0800

    knn,nn

--- digitrecognizer/NN.py
+++ digitrecognizer/NN.py
@@ -1,31 +1,5 @@
 import math
-from operator import itemgetter
-
-def readtrainset(filename):
-    f = open(filename, 'r')
-    trains = []
-    label = []
-    next(f)
-    for line in f:
-        strs = line.split(',')
-        nums = [int(x) for x in strs]
-        label.append(nums[0])
-        trains.append(nums[1:])
-    f.close()
-    return trains, label
-
-
-def readtestset(filename):
-    f = open(filename, 'r')
-    test = []
-
-    next(f)
-    for line in f:
-        strs = line.split(',')
-        nums = [int(x) for x in strs]
-        test.append(nums)
-    f.close()
-    return test
+import tools
 
 
 def classify(test, train, label):
@@ -42,8 +16,8 @@ def classify(test, train, label):
     return label[min]
 
 
-train, label = readtrainset('E:/Kaggle/DigitRecog/train1k.csv')
-test = readtestset('E:/Kaggle/DigitRecog/test.csv')
+train, label = tools.readtrainset('E:/Kaggle/DigitRecog/train1k.csv')
+test = tools.readtestset('E:/Kaggle/DigitRecog/test.csv')
 
 f = open('E:/Kaggle/DigitRecog/submission1.csv', 'a')
 f.write('ImageId,Label\n')
--- digitrecognizer/knn.py
+++ digitrecognizer/knn.py
@@ -1,31 +1,6 @@
 import math
 from operator import itemgetter
-
-def readtrainset(filename):
-    f = open(filename, 'r')
-    trains = []
-    label = []
-    next(f)
-    for line in f:
-        strs = line.split(',')
-        nums = [int(x) for x in strs]
-        label.append(nums[0])
-        trains.append(nums[1:])
-    f.close()
-    return trains, label
-
-
-def readtestset(filename):
-    f = open(filename, 'r')
-    test = []
-
-    next(f)
-    for line in f:
-        strs = line.split(',')
-        nums = [int(x) for x in strs]
-        test.append(nums)
-    f.close()
-    return test
+import tools
 
 
 def classify(test, train, label, k):
@@ -46,8 +21,8 @@ def classify(test, train, label, k):
         return k
 
 
-train, label = readtrainset('E:/Kaggle/DigitRecog/train1k.csv')
-test = readtestset('E:/Kaggle/DigitRecog/test.csv')
+train, label = tools.readtrainset('E:/Kaggle/DigitRecog/train1k.csv')
+test = tools.readtestset('E:/Kaggle/DigitRecog/test.csv')
 
 f = open('E:/Kaggle/DigitRecog/submission1.csv', 'a')
 f.write('ImageId,Label\n')
